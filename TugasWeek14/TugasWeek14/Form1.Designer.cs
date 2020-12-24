9eb7a032f6dfc75920429035478af23905ea95ff
Bridged Livia <bgunawan03@student.ciputra.ac.id>
2020-12-25 00:03:30 +0700

    TugaskWeek14

---
+++ TugasWeek14/TugasWeek14/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace TugasWeek14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelmasukal = new System.Windows.Forms.Label();
+            this.labelmasukh = new System.Windows.Forms.Label();
+            this.labelmenjadi = new System.Windows.Forms.Label();
+            this.textkalimat = new System.Windows.Forms.TextBox();
+            this.textboxhuruf = new System.Windows.Forms.TextBox();
+            this.textboxjadi = new System.Windows.Forms.TextBox();
+            this.buttonkonversi = new System.Windows.Forms.Button();
+            this.labelh = new System.Windows.Forms.Label();
+            this.labelhasil = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelmasukal
+            // 
+            this.labelmasukal.AutoSize = true;
+            this.labelmasukal.Location = new System.Drawing.Point(44, 36);
+            this.labelmasukal.Name = "labelmasukal";
+            this.labelmasukal.Size = new System.Drawing.Size(138, 20);
+            this.labelmasukal.TabIndex = 0;
+            this.labelmasukal.Text = "Masukkan Kalimat";
+            // 
+            // labelmasukh
+            // 
+            this.labelmasukh.AutoSize = true;
+            this.labelmasukh.Location = new System.Drawing.Point(44, 86);
+            this.labelmasukh.Name = "labelmasukh";
+            this.labelmasukh.Size = new System.Drawing.Size(115, 20);
+            this.labelmasukh.TabIndex = 1;
+            this.labelmasukh.Text = "Masukan huruf";
+            this.labelmasukh.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelmenjadi
+            // 
+            this.labelmenjadi.AutoSize = true;
+            this.labelmenjadi.Location = new System.Drawing.Point(327, 86);
+            this.labelmenjadi.Name = "labelmenjadi";
+            this.labelmenjadi.Size = new System.Drawing.Size(64, 20);
+            this.labelmenjadi.TabIndex = 2;
+            this.labelmenjadi.Text = "Menjadi";
+            // 
+            // textkalimat
+            // 
+            this.textkalimat.Location = new System.Drawing.Point(203, 36);
+            this.textkalimat.Name = "textkalimat";
+            this.textkalimat.Size = new System.Drawing.Size(309, 26);
+            this.textkalimat.TabIndex = 3;
+            // 
+            // textboxhuruf
+            // 
+            this.textboxhuruf.Location = new System.Drawing.Point(203, 86);
+            this.textboxhuruf.Name = "textboxhuruf";
+            this.textboxhuruf.Size = new System.Drawing.Size(100, 26);
+            this.textboxhuruf.TabIndex = 4;
+            // 
+            // textboxjadi
+            // 
+            this.textboxjadi.Location = new System.Drawing.Point(412, 86);
+            this.textboxjadi.Name = "textboxjadi";
+            this.textboxjadi.Size = new System.Drawing.Size(100, 26);
+            this.textboxjadi.TabIndex = 5;
+            // 
+            // buttonkonversi
+            // 
+            this.buttonkonversi.Location = new System.Drawing.Point(203, 157);
+            this.buttonkonversi.Name = "buttonkonversi";
+            this.buttonkonversi.Size = new System.Drawing.Size(309, 38);
+            this.buttonkonversi.TabIndex = 6;
+            this.buttonkonversi.Text = "Konversi!";
+            this.buttonkonversi.UseVisualStyleBackColor = true;
+            this.buttonkonversi.Click += new System.EventHandler(this.buttonkonversi_Click);
+            // 
+            // labelh
+            // 
+            this.labelh.AutoSize = true;
+            this.labelh.Location = new System.Drawing.Point(44, 234);
+            this.labelh.Name = "labelh";
+            this.labelh.Size = new System.Drawing.Size(48, 20);
+            this.labelh.TabIndex = 7;
+            this.labelh.Text = "Hasil:";
+            // 
+            // labelhasil
+            // 
+            this.labelhasil.AutoSize = true;
+            this.labelhasil.Location = new System.Drawing.Point(199, 234);
+            this.labelhasil.Name = "labelhasil";
+            this.labelhasil.Size = new System.Drawing.Size(0, 20);
+            this.labelhasil.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelhasil);
+            this.Controls.Add(this.labelh);
+            this.Controls.Add(this.buttonkonversi);
+            this.Controls.Add(this.textboxjadi);
+            this.Controls.Add(this.textboxhuruf);
+            this.Controls.Add(this.textkalimat);
+            this.Controls.Add(this.labelmenjadi);
+            this.Controls.Add(this.labelmasukh);
+            this.Controls.Add(this.labelmasukal);
+            this.Name = "Form1";
+            this.Text = "QUIZ PANDA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelmasukal;
+        private System.Windows.Forms.Label labelmasukh;
+        private System.Windows.Forms.Label labelmenjadi;
+        private System.Windows.Forms.TextBox textkalimat;
+        private System.Windows.Forms.TextBox textboxhuruf;
+        private System.Windows.Forms.TextBox textboxjadi;
+        private System.Windows.Forms.Button buttonkonversi;
+        private System.Windows.Forms.Label labelh;
+        private System.Windows.Forms.Label labelhasil;
+    }
+}
+
---
+++ TugasWeek14/TugasWeek14/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TugasWeek14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonkonversi_Click(object sender, EventArgs e)
+        {
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            char[] alphachar = new char[alpha.Length];
+            char[] charkalimat = new char[textkalimat.TextLength];
+            char[] texthuruf = new char[textboxhuruf.TextLength];
+            char[] texthuruf2 = new char[textboxjadi.TextLength];
+            string result = "";
+            int key = 0;
+            int counter1 = 0;
+            int counter2 = 0;
+            for(int i=0; i<alphachar.Length; i++)
+            {
+               if (texthuruf.Equals(alphachar[i]))
+                { counter1 = i; }
+
+               if(texthuruf2.Equals(alphachar[i]))
+                {
+                    counter2 = i;
+                
+                }
+                    
+            }
+
+            if(counter2>counter1)
+            {
+                key = counter2 - counter1;
+            }
+
+            else
+            { 
+                
+                key = counter2 + 26 - counter1; 
+            
+            }
+
+            for(int i=0; i<charkalimat.Length; i++)
+            {
+                if (charkalimat[i].Equals(""))
+                { result = result + " "; }
+
+                else
+                {
+                    for(int j=0; j<alphachar.Length; j++)
+                    { 
+                        if(charkalimat[i].Equals(alphachar[j]))
+                        { 
+                         if(j+key<26)
+                            {
+                                result = result + alphachar[j + key];
+                            }
+                         else
+                            {
+                                result = result + alphachar[j + key - 26];
+                            }
+                        }  
+                     }
+                }
+                
+            }
+            labelhasil.Text = result;
+
+        }
+    }
+}
